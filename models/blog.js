b8397d60f406feb5ad19a8968ebdc70e73141a1b
Josue Mixalis <cacciator31@gmail.com>
2024-02-21 20:02:14 -0600

    part 4.2 is done

---
+++ models/blog.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: {
+    type:String,
+    validate: {
+      validator: (v) => {
+        return /https:\/\/.*/.test(v)
+      },
+      message: props => `${props.value} is not a valid url!`
+    },
+    require:[true, 'the url is requiered']
+  },
+  likes:Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document,returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog',blogSchema)
